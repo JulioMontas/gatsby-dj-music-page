729fc2861d8a4b581f822cb4f38457fc600ae675
JulioMontas <julio86@gmail.com>
2022-09-21 18:55:15 -0400

    homepage style + basic seo

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,16 +2,17 @@ import * as React from "react"
 import { Link } from 'gatsby'
 
 const Footer = () => (
-  <div className="bg-[#393939] h-[5vh]">
-    <div className="flex items-center justify-around text-[#FFEA00]">
 
-      <h1 className="text-2xl font-bold">
+  <div className="bg-[#393939] h-[5vh]">
+    <div className="flex items-center justify-around text-[#FFEA00] font-bold uppercase tracking-widest">
+
+      <h1 className="text-2xl">
         <Link to="/" className="hover:underline">
-          M O N T A S
+          M O N T Á S
         </Link>
       </h1>
 
-      <ul className="flex uppercase tracking-wide">
+      <ul className="flex">
         <li className="px-6 ...">
           <Link className="hover:underline" to="/mixes">Mixes</Link>
         </li>
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,13 +7,13 @@ export default function IndexPage() {
     <>
     <div className="grid grid-cols-1 place-items-center bg-[#393939] h-[95vh]">
       <Helmet>
-        <title>Music Artist Website</title>
-        <meta name="description" content="Music Artist Website" />
+        <title>M O N T Á S • Artist DJ - Minimal House & Dub Techno</title>
+        <meta name="description" content="Originally from the Dominican Republic now based in NYC." />
         <link rel="canonical" href="https://montas.nyc/" />
       </Helmet>
       <div>
-      <svg height="250" width="500">
-        <polygon points="220,10 300,210 170,250 123,234" fill="#FFEA00"/>
+      <svg height="240" width="500">
+        <polygon points="220,10 300,210 123,234" fill="#FFEA00"/>
       </svg>
       </div>
     </div>
