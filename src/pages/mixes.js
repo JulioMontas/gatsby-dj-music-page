b03a002474463eec1564f6d960dd7aaf53621f64
JulioMontas <julio86@gmail.com>
2022-09-21 22:00:00 -0400

    remove text

--- src/pages/mixes.js
+++ src/pages/mixes.js
@@ -13,13 +13,13 @@ export default function MixesPage() {
       </Helmet>
       <ul className="flex gap-[2.75rem]">
         <li className="">
-          <Link className="block bg-[rgba(216,216,216,0.10)] border-2 border-[#ECD905] h-[80vh] w-72 rounded-tl-[195.5px] rounded-tr-[10px] rounded-br-[195.5px] rounded-bl-[30px]" to="/mixes">01</Link>
+          <Link className="block bg-[rgba(216,216,216,0.10)] border-2 border-[#ECD905] h-[75vh] w-72 rounded-tl-[195.5px] rounded-tr-[10px] rounded-br-[195.5px] rounded-bl-[30px]" to="/mixes"></Link>
         </li>
         <li className="">
-          <Link className="block bg-[rgba(216,216,216,0.10)] border-2 border-[#ECD905] h-[80vh] w-72 rounded-tl-[195.5px] rounded-tr-[195.5px] rounded-br-[10px] rounded-bl-[108px]" to="/mixes">02</Link>
+          <Link className="block bg-[rgba(216,216,216,0.10)] border-2 border-[#ECD905] h-[75vh] w-72 rounded-tl-[195.5px] rounded-tr-[195.5px] rounded-br-[10px] rounded-bl-[108px]" to="/mixes"></Link>
         </li>
         <li className="">
-          <Link className="block bg-[rgba(216,216,216,0.10)] border-2 border-[#ECD905] h-[80vh] w-72 rounded-tl-[0] rounded-tr-[195.5px] rounded-br-[29px] rounded-bl-[195.5px]" to="/mixes">03</Link>
+          <Link className="block bg-[rgba(216,216,216,0.10)] border-2 border-[#ECD905] h-[75vh] w-72 rounded-tl-[0] rounded-tr-[195.5px] rounded-br-[29px] rounded-bl-[195.5px]" to="/mixes"></Link>
         </li>
       </ul>
     </Layout>
