b1e7c7c811b04bc5e310821a4494a02aa2881676
JulioMontas <julio86@gmail.com>
2022-09-22 00:18:53 -0400

    style

---
+++ src/pages/mixes/index.js
@@ -0,0 +1,27 @@
+import * as React from "react"
+import {Helmet} from "react-helmet";
+import Layout from "../../components/layout"
+import { Link } from 'gatsby'
+
+export default function MixesPage() {
+  return (
+    <Layout>
+      <Helmet>
+        <title>Mixes • DJ & Producer - Minimal Electronic Music</title>
+        <meta name="description" content="From the Dominican Republic based in NYC." />
+        <link rel="canonical" href="https://montas.nyc/mixes" />
+      </Helmet>
+      <ul className="flex gap-[2.75rem]">
+        <li className="">
+          <Link className="block bg-[rgba(216,216,216,0.10)] border-2 border-[#ECD905] h-[75vh] w-80 rounded-tl-[195.5px] rounded-tr-[10px] rounded-br-[195.5px] rounded-bl-[30px] hover:border-[#333] transition-all duration-700" to="/mixes/01"></Link>
+        </li>
+        <li className="">
+          <Link className="block bg-[rgba(216,216,216,0.10)] border-2 border-[#ECD905] h-[75vh] w-72 rounded-tl-[195.5px] rounded-tr-[195.5px] rounded-br-[10px] rounded-bl-[108px] hover:border-[#333] transition-all duration-700" to="/mixes/01"></Link>
+        </li>
+        <li className="">
+          <Link className="block bg-[rgba(216,216,216,0.10)] border-2 border-[#ECD905] h-[75vh] w-72 rounded-tl-[0] rounded-tr-[195.5px] rounded-br-[29px] rounded-bl-[195.5px] hover:border-[#333] transition-all duration-700" to="/mixes/01"></Link>
+        </li>
+      </ul>
+    </Layout>
+  )
+}
