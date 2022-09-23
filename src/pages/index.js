4b86f433104abb72100ee71593db8f6ff3f6e2a4
JulioMontas <julio86@gmail.com>
2022-09-23 18:21:47 -0400

    homepage video

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import * as React from "react"
 import {Helmet} from "react-helmet";
 import Layout from "../components/layout"
+import DogVideo from "../assets/babysquare.mp4"
 
 export default function IndexPage() {
   return (
@@ -11,9 +12,21 @@ export default function IndexPage() {
         <link rel="canonical" href="https://montas.nyc/" />
       </Helmet>
 
+      {/*
       <svg height="240" width="400">
         <polygon points="220,10 300,210 123,234" fill="#FFEA00"/>
       </svg>
+      */}
+
+      <video autoPlay loop style={{
+        right: `0`,
+        bottom: `0`,
+        minWidth: `100%`,
+        minHeight: `100%`,
+      }}>
+        <source src={DogVideo} type="video/mp4" />
+        Your browser does not support HTML5 video.
+      </video>
     </Layout>
   )
 }
