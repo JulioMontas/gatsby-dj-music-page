95eb9f6fad938fe264ad05d965f6495e87d034a9
JulioMontas <julio86@gmail.com>
2022-10-01 15:36:24 -0400

    Scroll improve

--- src/pages/about.js
+++ src/pages/about.js
@@ -53,17 +53,20 @@ export default function AboutPage() {
         />
       </svg>
       <motion.div exit={{ opacity: 0 }} initial='initial' animate='animate'>
-        <motion.div variants={stagger} ref={ref} className="sm:h-[100vh] lg:h-[auto] flex flex-col gap-[20px] overflow-y-scroll ...">
+        <motion.div variants={stagger} ref={ref} className="h-[100vh] flex flex-col gap-[20px] pt-20 pb-40 overflow-y-scroll ...">
 
           <motion.p variants={fadeInUp} className="sm:text-xl lg:text-5xl lg:leading-[5rem] sm:leading-[2.2rem] text-[#facc15] font-bold">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque lectus libero, finibus et consectetur. Curabitur pharetra ac turpis ac consectetur pellentesque lobortis upretit Pellentesque lectus libero, finibus et consectetur. Curabitur pharetra ac turpis ac consectetur pellentesque lobortis upretit
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque lectus libero, finibus et consectetur. Curabitur pharetra ac turpis ac consectetur pellentesque lobortis upretit Pellentesque lectus libero, finibus et consectetur. Curabitur pharetra ac turpis ac consectetur pellentesque lobortis upretit
           </motion.p>
 
           <motion.p variants={fadeInUp} className="sm:text-l lg:text-4xl lg:leading-[4rem] sm:leading-[1.8rem] text-[#fde047] font-medium">
             faucibus. Cras ornare arcu vitae neque lobortis varius. Vivamus varius ullamcorper leo, vel sollicitudin augue varius at. Proin lobortis vel orci bibendum facilisis.
+            faucibus. Cras ornare arcu vitae neque lobortis varius. Vivamus varius ullamcorper leo, vel sollicitudin augue varius at. Proin lobortis vel orci bibendum facilisis.
           </motion.p>
 
           <motion.p variants={fadeInUp} className="sm:text-s lg:text-2xl lg:leading-[3rem] sm:leading-[1.7rem] text-[#fef08a] font-light">
+            EEtiam nec facilisis mauris. Suspendisse dictum ipsum magna, eget tincidunt arcu interdum in. Etiam tristique tincidunt congue. Proin ac sodales velit. Vivamus varius ullamcorper leo, vel sollicitudin augue Cras ornare arcu vitae neque lobortis varius curabitur neque
             EEtiam nec facilisis mauris. Suspendisse dictum ipsum magna, eget tincidunt arcu interdum in. Etiam tristique tincidunt congue. Proin ac sodales velit. Vivamus varius ullamcorper leo, vel sollicitudin augue Cras ornare arcu vitae neque lobortis varius curabitur neque
           </motion.p>
 
